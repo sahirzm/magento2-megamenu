b6a03d678642a8db5386681277575cfd8622d3e4
Sahir Maredia <sahirzm@gmail.com>
2017-01-27 15:03:08 -0500

    configured admin menu item

---
+++ Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,28 @@
+<?php
+namespace Sahir\MegaMenu\Controller\Adminhtml\Index;
+class Index extends \Magento\Backend\App\Action
+{
+    protected $resultPageFactory;
+
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory)
+    {
+        $this->resultPageFactory = $resultPageFactory;        
+        return parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        $page = $this->resultPageFactory->create();  
+        $page->setActiveMenu('Sahir_MegaMenu::sahir_megamenu_menu');
+        $page->getConfig()->getTitle()->prepend(__('Configure Megamenu'));
+        return $page;
+    }    
+
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('Sahir_MegaMenu::config');            
+    }
+
+}
